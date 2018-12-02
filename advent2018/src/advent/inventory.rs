4cfb09a6b0ab0161ed3b43794c6c88ed029e0237
Kyle Isom <kyle@imap.cc>
2018-12-02 02:03:55 -0800

    Run cargo fmt.

--- advent2018/src/advent/inventory.rs
+++ advent2018/src/advent/inventory.rs
@@ -1,146 +1,146 @@
 fn compute_checksum(box_id: String) -> (i64, i64) {
-	let mut set: [u8; 26] = [
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	];
-	let mut two: i64 = 0;
-	let mut three: i64 = 0;
+    let mut set: [u8; 26] = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ];
+    let mut two: i64 = 0;
+    let mut three: i64 = 0;
 
-	for c in box_id.chars() {
-		let i = c as usize;
-		set[i - 0x61] += 1;
-	}
+    for c in box_id.chars() {
+        let i = c as usize;
+        set[i - 0x61] += 1;
+    }
 
-	for i in 0..set.len() {
-		if set[(i as usize)] == 2 {
-			two += 1;
-		}
-		if set[i as usize] == 3 {
-			three += 1;
-		}
-	}
+    for i in 0..set.len() {
+        if set[(i as usize)] == 2 {
+            two += 1;
+        }
+        if set[i as usize] == 3 {
+            three += 1;
+        }
+    }
 
-	if two > 0 {
-		two = 1;
-	}
+    if two > 0 {
+        two = 1;
+    }
 
-	if three > 0 {
-		three = 1;
-	}
+    if three > 0 {
+        three = 1;
+    }
 
-	return (two, three);
+    return (two, three);
 }
 
 #[test]
 fn test_compute_checksum() {
-	let (two, three) = compute_checksum("abcdef".to_string());
-	assert!(two == 0 && three == 0);
+    let (two, three) = compute_checksum("abcdef".to_string());
+    assert!(two == 0 && three == 0);
 
-	let (two, three) = compute_checksum("bababc".to_string());
-	assert!(two == 1 && three == 1);
+    let (two, three) = compute_checksum("bababc".to_string());
+    assert!(two == 1 && three == 1);
 }
 
 pub fn compute_checksums(box_ids: &Vec<String>) -> i64 {
-	let mut two: i64 = 0;
-	let mut three: i64 = 0;
+    let mut two: i64 = 0;
+    let mut three: i64 = 0;
 
-	for box_id in box_ids.iter() {
-		let (two_, three_) = compute_checksum(box_id.to_string());
-		two += two_;
-		three += three_;
-	}
+    for box_id in box_ids.iter() {
+        let (two_, three_) = compute_checksum(box_id.to_string());
+        two += two_;
+        three += three_;
+    }
 
-	return two * three;
+    return two * three;
 }
 
 #[test]
 fn test_compute_checksums() {
-	let box_ids: Vec<String> = vec![
-		"abcdef".to_string(),
-		"bababc".to_string(),
-		"abbcde".to_string(),
-		"abcccd".to_string(),
-		"aabcdd".to_string(),
-		"abcdee".to_string(),
-		"ababab".to_string(),
-	];
-	let checksum = compute_checksums(&box_ids);
-	assert!(checksum == 12);
+    let box_ids: Vec<String> = vec![
+        "abcdef".to_string(),
+        "bababc".to_string(),
+        "abbcde".to_string(),
+        "abcccd".to_string(),
+        "aabcdd".to_string(),
+        "abcdee".to_string(),
+        "ababab".to_string(),
+    ];
+    let checksum = compute_checksums(&box_ids);
+    assert!(checksum == 12);
 }
 
 fn match_boxes(box_id1: String, box_id2: String) -> Option<String> {
-	let mut differences = 0;
-	let mut common: String = String::new();
+    let mut differences = 0;
+    let mut common: String = String::new();
 
-	let box_id1_chars = box_id1.as_bytes();
-	let box_id2_chars = box_id2.as_bytes();
+    let box_id1_chars = box_id1.as_bytes();
+    let box_id2_chars = box_id2.as_bytes();
 
-	for i in 0..box_id1.len() {
-		if box_id1_chars[i] == box_id2_chars[i] {
-			common.push(box_id1_chars[i] as char);
-			continue;
-		}
+    for i in 0..box_id1.len() {
+        if box_id1_chars[i] == box_id2_chars[i] {
+            common.push(box_id1_chars[i] as char);
+            continue;
+        }
 
-		differences += 1;
-		if differences > 1 {
-			break;
-		}
-	}
+        differences += 1;
+        if differences > 1 {
+            break;
+        }
+    }
 
-	if differences == 1 {
-		return Some(common);
-	}
+    if differences == 1 {
+        return Some(common);
+    }
 
-	None
+    None
 }
 
 #[test]
 fn test_match_boxes() {
-	assert!(
-		match_boxes("abcde".to_string(), "fghij".to_string()).is_none(),
-		"boxes shouldn't match"
-	);
-	assert!(
-		match_boxes("abcde".to_string(), "axcye".to_string()).is_none(),
-		"boxes shouldn't match"
-	);
-	let common = match_boxes("fghij".to_string(), "fguij".to_string());
-	match common {
-		Some(s) => {
-			println!("common: {}", s);
-			assert!(s == "fgij".to_string());
-		}
-		_ => assert!(false),
-	};
+    assert!(
+        match_boxes("abcde".to_string(), "fghij".to_string()).is_none(),
+        "boxes shouldn't match"
+    );
+    assert!(
+        match_boxes("abcde".to_string(), "axcye".to_string()).is_none(),
+        "boxes shouldn't match"
+    );
+    let common = match_boxes("fghij".to_string(), "fguij".to_string());
+    match common {
+        Some(s) => {
+            println!("common: {}", s);
+            assert!(s == "fgij".to_string());
+        }
+        _ => assert!(false),
+    };
 }
 
 pub fn match_inventory(inventory: &Vec<String>) -> Option<String> {
-	for i in 0..(inventory.len() - 1) {
-		for j in i + 1..inventory.len() {
-			let result = match_boxes(inventory[i].clone(), inventory[j].clone());
-			match result {
-				Some(common) => return Some(common),
-				_ => continue,
-			}
-		}
-	}
+    for i in 0..(inventory.len() - 1) {
+        for j in i + 1..inventory.len() {
+            let result = match_boxes(inventory[i].clone(), inventory[j].clone());
+            match result {
+                Some(common) => return Some(common),
+                _ => continue,
+            }
+        }
+    }
 
-	None
+    None
 }
 
 #[test]
 fn test_match_inventory() {
-	let inventory = &vec![
-		"abcde".to_string(),
-		"fghij".to_string(),
-		"klmno".to_string(),
-		"pqrst".to_string(),
-		"fguij".to_string(),
-		"axcye".to_string(),
-		"wvxyz".to_string(),
-	];
-	let result = match_inventory(&inventory);
-	match result {
-		Some(common) => assert!(common == "fgij".to_string()),
-		_ => assert!(false),
-	};
+    let inventory = &vec![
+        "abcde".to_string(),
+        "fghij".to_string(),
+        "klmno".to_string(),
+        "pqrst".to_string(),
+        "fguij".to_string(),
+        "axcye".to_string(),
+        "wvxyz".to_string(),
+    ];
+    let result = match_inventory(&inventory);
+    match result {
+        Some(common) => assert!(common == "fgij".to_string()),
+        _ => assert!(false),
+    };
 }
--- advent2018/src/advent/util.rs
+++ advent2018/src/advent/util.rs
@@ -3,26 +3,26 @@ use std::io::BufRead;
 use std::io::BufReader;
 
 pub fn read_int_sequence(filename: String) -> Vec<i64> {
-	let mut deltas = Vec::new();
-	let file = File::open(filename).expect("file not found");
-	let reader = BufReader::new(&file);
+    let mut deltas = Vec::new();
+    let file = File::open(filename).expect("file not found");
+    let reader = BufReader::new(&file);
 
-	for line in reader.lines() {
-		let delta: i64 = line.unwrap().parse().unwrap();
-		deltas.push(delta);
-	}
+    for line in reader.lines() {
+        let delta: i64 = line.unwrap().parse().unwrap();
+        deltas.push(delta);
+    }
 
-	return deltas;
+    return deltas;
 }
 
 pub fn read_string_sequence(filename: String) -> Vec<String> {
-	let mut strings = Vec::new();
-	let file = File::open(filename).expect("file not found");
-	let reader = BufReader::new(&file);
+    let mut strings = Vec::new();
+    let file = File::open(filename).expect("file not found");
+    let reader = BufReader::new(&file);
 
-	for line in reader.lines() {
-		strings.push(line.unwrap());
-	}
+    for line in reader.lines() {
+        strings.push(line.unwrap());
+    }
 
-	return strings;
+    return strings;
 }
